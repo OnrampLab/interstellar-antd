5440599043b21f75cf52d2584ae9032feab4e413
Kos Huang <koshuang@gmail.com>
2024-03-13 14:08:56 +0800

    chore(component): [message] change xpath

--- src/interstellar_antd/components/message.py
+++ src/interstellar_antd/components/message.py
@@ -4,7 +4,7 @@ from interstellar.framework import Element
 
 
 class Message(Element):
-    XPATH_CURRENT = '//div[@class="ant-message"]'
+    XPATH_CURRENT = '//div[@class="ant-message"]/div/div'
 
     def get_content(self):
-        return self.find_dom_element_by_xpath(f"./div/div").text
+        return self.get_current_dom_element().text
