7d51dbe9ae04ea74854d2e0f714a5c9893ca9d0a
Kos Huang <koshuang@gmail.com>
2024-03-27 17:44:37 +0800

    test(helper): [pytest_generate_tests_helper] new for testing scenarios for v4 and v5 components

---
+++ tests/pytest_generate_tests_helper.py
@@ -0,0 +1,9 @@
+def pytest_generate_tests_helper(metafunc):
+    idlist = []
+    argvalues = []
+    for scenario in metafunc.cls.scenarios:
+        idlist.append(scenario[0])
+        items = scenario[1].items()
+        argnames = [x[0] for x in items]
+        argvalues.append([x[1] for x in items])
+    metafunc.parametrize(argnames, argvalues, ids=idlist, scope="class")
